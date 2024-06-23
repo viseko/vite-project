baa330f7e742f677dfd361c693af24a29bc7cb0c
Viktor Konovalov <viseko23@gmail.com>
2024-06-23 15:25:38 +0700

    Temporally shitcode

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,10 +3,18 @@ import { Header } from "../widgets/Header"
 import useVh from "./hooks/useVh"
 
 import "./styles/index.scss"
-import "./ui/App.scss"
+import "./ui/App.scss";
+
+function SomeShit () {
+  return <div>
+
+    <h1>KLWKLE</h1>
+  </div>
+};
 
 function App() {
   const vh = useVh();
+  let a = 1
 
   return (
     <div className="app" style={{
@@ -15,6 +23,7 @@ function App() {
       <Header className="app__header"/>
       <main className="app__main">
         <div className="container">
+          { a > 0 && <SomeShit />}
         </div>
       </main>
       <Footer />
